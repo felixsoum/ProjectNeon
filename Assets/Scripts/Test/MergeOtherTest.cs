d7d6f33a45e807deff9935b62caa8fa0714be357
felixsoum <felix@felixsoum.com>
2016-07-13 22:33:32 -0400

    Add scene merge test

---
+++ Assets/Scripts/Test/MergeOtherTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MergeOtherTest : MonoBehaviour
+{
+	void Start()
+    {
+        Debug.Log("I'm active");
+        var playerObject = GameObject.FindGameObjectWithTag("Player");
+        playerObject.transform.localScale *= 2f;
+	}
+}
---
+++ Assets/Scripts/Test/MergeTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MergeTest : MonoBehaviour
+{
+	void Start()
+    {
+        SceneManager.LoadSceneAsync("MergeSource", LoadSceneMode.Additive);
+        Invoke("Unload", 5f);
+    }
+
+    void Unload()
+    {
+        SceneManager.UnloadScene("MergeSource");
+    }
+}
