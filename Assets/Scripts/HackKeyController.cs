f329d485e66e9c56ec7b1db15d1c50043aa226b3
felixsoum <felix@felixsoum.com>
2016-08-07 01:26:51 -0400

    Add base hack rhythm

---
+++ Assets/Scripts/HackKeyController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HackKeyController : MonoBehaviour
+{
+    public HackKeySystem system;
+    Text keyText;
+    const float MOVE_SPEED = 200f;
+    const float SPAWN_Y = 240f;
+    const float UNSPAWN_Y = -240f;
+    const float TARGET_Y = -200f;
+    public char keyChar = 'X';
+
+    const float A_POS_X= -53.33f;
+    const float S_POS_X = 0;
+    const float D_POS_X = 53.33f;
+    bool hasPlayedRhythmSound;
+
+	void Start()
+    {
+        keyText = GetComponent<Text>();
+        system.AddHackKey(this);
+        gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        Vector2 pos = keyText.rectTransform.localPosition;
+        pos.y -= MOVE_SPEED * Time.deltaTime;
+        keyText.rectTransform.localPosition = pos;
+
+        if (!hasPlayedRhythmSound && pos.y <= TARGET_Y)
+        {
+            hasPlayedRhythmSound = true;
+            system.SoundRhythm();
+        }
+
+        if (pos.y <= UNSPAWN_Y)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+
+    public void Spawn()
+    {
+        hasPlayedRhythmSound = false;
+        int randomInt = Random.Range(0, 3);
+        float posX;
+        switch (randomInt)
+        {
+            case 0:
+                keyChar = 'A';
+                posX = A_POS_X;
+                break;
+            case 1:
+                keyChar = 'S';
+                posX = S_POS_X;
+                break;
+            case 2:
+            default:
+                keyChar = 'D';
+                posX = D_POS_X;
+                break;
+        }
+        gameObject.SetActive(true);
+        Vector2 pos = keyText.rectTransform.localPosition;
+        pos.x = posX;
+        pos.y = SPAWN_Y;
+        keyText.rectTransform.localPosition = pos;
+        keyText.text = keyChar.ToString();
+    }
+
+    public float GetY()
+    {
+        return keyText.rectTransform.localPosition.y;
+    }
+
+    public void Hit()
+    {
+        keyText.text = "";
+    }
+}
---
+++ Assets/Scripts/HackKeySystem.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class HackKeySystem : MonoBehaviour
+{
+    public Text comboText;
+    public AudioSource rhythmAudio;
+    public AudioSource hitAudio;
+    List<HackKeyController> hackKeys;
+    const float hackKeySpawnPeriod = 0.5f;
+    float hackKeySpawnTime;
+    int spawnIndex;
+    Random random;
+    int comboCount;
+
+    const float targetY = -200f;
+    const float targetHitRange = 25f;
+
+    void Awake()
+    {
+        hackKeys = new List<HackKeyController>();
+    }
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("SpawnHackKey", hackKeySpawnPeriod, hackKeySpawnPeriod);
+        comboText.gameObject.SetActive(false);
+	}
+	
+	void Update()
+    {
+        char inputChar = 'X';
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            inputChar = 'A';
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            inputChar = 'S';
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            inputChar = 'D';
+        }
+        if (inputChar != 'X')
+        {
+            bool isSuccess = false;
+            foreach (HackKeyController key in hackKeys)
+            {
+                if (key.gameObject.activeInHierarchy && key.keyChar == inputChar)
+                {
+                    float distanceToTarget = Mathf.Abs(key.GetY() - targetY);
+                    if (distanceToTarget < targetHitRange)
+                    {
+                        isSuccess = true;
+                        comboCount++;
+                        comboText.gameObject.SetActive(true);
+                        key.Hit();
+                        hitAudio.Play();
+                        break;
+                    }
+                }
+            }
+            if (!isSuccess)
+            {
+                comboCount = 0;
+                comboText.gameObject.SetActive(false);
+            }
+            comboText.text = "x" + comboCount.ToString();
+
+        }
+    }
+
+    public void AddHackKey(HackKeyController hackKey)
+    {
+        hackKeys.Add(hackKey);
+    }
+
+    void SpawnHackKey()
+    {
+        hackKeys[spawnIndex++].Spawn();
+        spawnIndex %= hackKeys.Count;
+    }
+
+    public void SoundRhythm()
+    {
+        rhythmAudio.Play();
+    }
+}
