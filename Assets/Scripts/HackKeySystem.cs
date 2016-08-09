360fd5efa1b25e8096903f6dcbefb33c981f0ed0
felixsoum <felix@felixsoum.com>
2016-08-08 22:02:12 -0400

    Add hit effects on the rhythm keys

--- Assets/Scripts/HackKeySystem.cs
+++ Assets/Scripts/HackKeySystem.cs
@@ -8,12 +8,18 @@ public class HackKeySystem : MonoBehaviour
     public Text comboText;
     public AudioSource rhythmAudio;
     public AudioSource hitAudio;
+
+    public Image aCircleImage;
+    public Image sCircleImage;
+    public Image dCircleImage;
+
     List<HackKeyController> hackKeys;
     const float hackKeySpawnPeriod = 0.5f;
     float hackKeySpawnTime;
     int spawnIndex;
     Random random;
     int comboCount;
+    const float CIRCLE_FADE_SPEED = 2f;
 
     const float targetY = -200f;
     const float targetHitRange = 25f;
@@ -59,6 +65,7 @@ public class HackKeySystem : MonoBehaviour
                         comboText.gameObject.SetActive(true);
                         key.Hit();
                         hitAudio.Play();
+                        OnKeyHit(inputChar);
                         combat.RangeAttack();
                         break;
                     }
@@ -70,8 +77,11 @@ public class HackKeySystem : MonoBehaviour
                 comboText.gameObject.SetActive(false);
             }
             comboText.text = "x" + comboCount.ToString();
-
         }
+
+        DecrementCircleAlpha(aCircleImage);
+        DecrementCircleAlpha(sCircleImage);
+        DecrementCircleAlpha(dCircleImage);
     }
 
     public void AddHackKey(HackKeyController hackKey)
@@ -88,5 +98,35 @@ public class HackKeySystem : MonoBehaviour
     public void SoundRhythm()
     {
         rhythmAudio.Play();
+    }
+
+    public void OnKeyHit(char key)
+    {
+        switch (key)
+        {
+            case 'A':
+                SetCircleAlpha(aCircleImage, 1.0f);
+                break;
+            case 'S':
+                SetCircleAlpha(sCircleImage, 1.0f);
+                break;
+            case 'D':
+                SetCircleAlpha(dCircleImage, 1.0f);
+                break;
+        }
+    }
+
+    void SetCircleAlpha(Image circle, float aValue)
+    {
+        Color color = circle.color;
+        color.a = aValue;
+        circle.color = color;
+    }
+
+    void DecrementCircleAlpha(Image circle)
+    {
+        Color color = circle.color;
+        color.a = Mathf.Max(color.a - CIRCLE_FADE_SPEED * Time.deltaTime, 0);
+        circle.color = color;
     }
 }
