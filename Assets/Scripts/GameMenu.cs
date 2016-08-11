093a86b4c1bb7e08017ae4010d7e88800bec7d12
blam <brianlamlook@gmail.com>
2016-08-11 19:48:06 -0400

    Add game end condition and menu

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+	public enum Animation
+	{
+		MenuFade,
+		MenuEnd,
+	}
+	public Animator menuAnimator = null;
+
+	public Text menuText = null;
+	public Button menuButton = null;
+
+	// Put all enemies under a parent and link it here, to detect when enemies are all dead
+	public GameObject enemyContainer = null;
+
+
+
+	void Start()
+	{
+		if( menuButton != null )
+		{
+			menuButton.gameObject.SetActive(false);
+		}
+	}
+
+	void Update()
+	{
+		if( enemyContainer != null && enemyContainer.transform.childCount == 0 )
+		{
+			if( menuAnimator != null )
+			{
+				menuAnimator.Play( Animation.MenuEnd.ToString() );
+				if( menuText != null && menuButton != null)
+				{
+					menuText.text = "SYSTEM CLEAN";
+					menuButton.gameObject.SetActive(true);
+				}
+			}
+		}
+	}
+
+	// The function only shows in the editor when it's public?
+	public void OnButton_Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+}
