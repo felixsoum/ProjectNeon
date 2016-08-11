20b538de3f3efaae78bba170a3cd52c9d052db90
felixsoum <felix@felixsoum.com>
2016-08-10 23:25:16 -0400

    Add HP

--- Assets/Scripts/DamageDetection.cs
+++ Assets/Scripts/DamageDetection.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 
 public class DamageDetection : MonoBehaviour
 {
+    const int PLAYER_HP_MAX = 5;
+    int playerHp = PLAYER_HP_MAX;
 	// Use unity game object tags to determine what damages what.
 	// Map character tag to a list of tags which will damage that character.
 	private Dictionary<string, List<string>> damage_tags = new Dictionary<string, List<string>>
@@ -27,13 +29,29 @@ public class DamageDetection : MonoBehaviour
 			//Ghetto handling
 			if(this.CompareTag("Player"))
 			{
-				// For now just teleport the object as if killed and respawned
-				this.transform.position = Vector3.zero;
-			}
+                if (--playerHp == 0)
+                {
+                    gameObject.SetActive(false);
+                    Invoke("Respawn", 3f);
+                }
+            }
 			else
 			{
 				Destroy(this.gameObject);
 			}
 		}
 	}
+
+    public int GetPlayerHp()
+    {
+        return playerHp;
+    }
+
+    void Respawn()
+    {
+        // For now just teleport the object as if killed and respawned
+        gameObject.SetActive(true);
+        this.transform.position = Vector3.zero;
+        playerHp = PLAYER_HP_MAX;
+    }
 }
---
+++ Assets/Scripts/HpUi.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HpUi : MonoBehaviour
+{
+    public DamageDetection playerDamage;
+    public RectTransform hpRect;
+    int maxHp;
+    float initialHeight;
+    float initialY;
+
+    void Start()
+    {
+        maxHp = playerDamage.GetPlayerHp();
+        initialHeight = hpRect.sizeDelta.y;
+        initialY = hpRect.anchoredPosition.y;
+    }
+
+
+	void Update()
+    {
+        float hpRatio = (float)playerDamage.GetPlayerHp() / maxHp;
+
+        float topValue = initialHeight * (1f - hpRatio);
+
+        Vector2 pos = hpRect.anchoredPosition;
+        pos.y = initialY - topValue/2f;
+        hpRect.anchoredPosition = pos;
+
+        Vector2 size = hpRect.sizeDelta;
+        size.y = initialHeight * hpRatio;
+        hpRect.sizeDelta = size;
+    }
+}
